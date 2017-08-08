b3e3a78d1a0789de2d275655e1ac8bfc927151e5
cmdalbem <ninja.dalbem@gmail.com>
2017-08-07 23:39:10 -0300

    adds label to social links on footer

--- app/js/app.js
+++ app/js/app.js
@@ -1742,15 +1742,15 @@ $(() => {
       }
     });
 
-    $('#facebook-social-link').on('click', () => {
+    $('.facebook-social-link').on('click', () => {
       ga('send', 'event', 'Misc', 'facebook hamburger menu link click');
     });
 
-    $('#instagram-social-link').on('click', () => {
+    $('.instagram-social-link').on('click', () => {
       ga('send', 'event', 'Misc', 'instagram hamburger menu link click');
     });
 
-    $('#github-social-link').on('click', () => {
+    $('.github-social-link').on('click', () => {
       ga('send', 'event', 'Misc', 'github hamburger menu link click');
     });
 
