9b18c653786855ef0a4804152bd425b8cd60a257
cmdalbem <ninja.dalbem@gmail.com>
2017-10-30 00:42:40 -0200

    dont show default GMaps marker on place search

--- app/js/app.js
+++ app/js/app.js
@@ -1112,6 +1112,8 @@ $(() => {
     autocomplete.addListener('place_changed', () => {
       // infowindow.close();
       _searchResultMarker.setVisible(false);
+      _searchResultMarker.setAnimation(null);
+
       const place = autocomplete.getPlace();
       if (!place.geometry) {
         console.error('Autocomplete\'s returned place contains no geometry');
@@ -1135,21 +1137,12 @@ $(() => {
       //   scaledSize: new google.maps.Size(35, 35)
       // }));
 
-      _searchResultMarker.setPosition(place.geometry.location);
-      _searchResultMarker.setVisible(true);
+      // Show temporary result marker
+      // _searchResultMarker.setPosition(place.geometry.location);
+      // _searchResultMarker.setVisible(true);
+      // _searchResultMarker.setAnimation(google.maps.Animation.DROP);
 
       ga('send', 'event', 'Search', 'location', place.formatted_address); 
-
-      // var address = '';
-      // if (place.address_components) {
-      //   address = [
-      //               (place.address_components[0] && place.address_components[0].short_name || ''),
-      //               (place.address_components[1] && place.address_components[1].short_name || ''),
-      //               (place.address_components[2] && place.address_components[2].short_name || '')
-      //   ].join(' ');
-      // }
-      // infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-      // infowindow.open(map, marker);
     });
   }
 
