ac3b8b94bfa1490db5fc247f770a4dea34e567ca
Cristiano M Dalbem <cristiano.dalbem@vtex.com.br>
2017-11-19 17:40:55 -0200

    super lightweight login redirect page

--- app/js/app.js
+++ app/js/app.js
@@ -2792,7 +2792,8 @@ $(() => {
         google: GOOGLE_CLIENT_ID, 
         // windows: WINDOWS_CLIENT_ID,
     },{
-      redirect_uri: window.location.origin
+      // redirect_uri: window.location.origin
+      redirect_uri: '/redirect.html'
     });
     hello.on('auth.login', auth => {
       // Hack to fix what I think is the bug that was causing duplicate user entries
