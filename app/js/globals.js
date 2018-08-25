85a3f8134b6c65d85e0e6a0ef8d1a5f25bc90b0f
Cristiano Dalbem <ninja.dalbem@gmail.com>
2018-08-25 19:47:37 -0300

    Add BDB_COUNTRYCODE global var and customize initial map pos and bounding boxes

--- app/js/globals.js
+++ app/js/globals.js
@@ -19,6 +19,16 @@ function getSimulatedDelay () {
 ///////////////
 // Constants //
 ///////////////
+
+const BDB_COUNTRYCODE = '<BDB_COUNTRYCODE>';
+
+const CAPITALS = {
+  'BR': { latitude: -30.0346, longitude: -51.2177 }, // Porto Alegre
+  'PT': { latitude: 38.736946, longitude: -9.142685 } // Lisbon
+};
+
+const DEFAULT_COORDS = CAPITALS[BDB_COUNTRYCODE];
+
 const N_MOCK_PICS = 14;
 
 const MAX_ZOOM_TO_SHOW_PINS = 14;
@@ -84,10 +94,18 @@ const MAX_NAME_SUGGESTIONS = 5;
 //DEFAULT_COORDS is pointing to Porto Alegre
 const DEFAULT_COORDS = { latitude: -30.0346, longitude: -51.2177 };
 
-const MAP_BOUNDS_COORDS = { 
-    sw: { lat: '-34.0526594796', lng: '-61.3037107971' }, 
-    ne: { lat: '0.1757808338', lng: '-34.3652340941' } 
+const countriesBoundingBoxes = {
+  'BR': {
+    sw: { lat: '-34.0526594796', lng: '-61.3037107971' },
+    ne: { lat: '0.1757808338', lng: '-34.3652340941' }
+  },
+  'PT': {
+    sw: { lat: '36.838268541', lng: '-9.52657060387' },
+    ne: { lat: '42.280468655', lng: '-6.3890876937' }
+  }
 };
+const MAP_BOUNDS_COORDS = countriesBoundingBoxes['<BDB_COUNTRYCODE>'];
+
 /////////////
 // Globals //
 /////////////
